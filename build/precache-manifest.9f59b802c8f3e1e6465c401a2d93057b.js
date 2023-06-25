e893bc91f48ebf51dacd3d184ebe51149aee2644
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-06-25 16:54:47 -0500

    Fixed Build Issue

---
+++ build/precache-manifest.9f59b802c8f3e1e6465c401a2d93057b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e05e1b30184d943d4a2e68dc533510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40bd340e4771f563e40f",
+    "url": "/static/css/main.ec0bb965.chunk.css"
+  },
+  {
+    "revision": "e74335656efa09d11d85",
+    "url": "/static/js/2.5056eda3.chunk.js"
+  },
+  {
+    "revision": "0d808394430246d78d523b2c311978ca",
+    "url": "/static/js/2.5056eda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bd340e4771f563e40f",
+    "url": "/static/js/main.e3267b7f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e3267b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5e4ace7e9c5a53bd8e",
+    "url": "/static/js/runtime-main.06e93911.js"
+  }
+]);
