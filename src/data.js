9e9485aaa6317332b6934b58594a1f3b04412784
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-06-25 16:22:34 -0500

    Commit 4

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "SportsApp",
+    subtitle: "Django | HTML/CSS | AWS | Postgres",
+    description: "Designed a real-time NBA platform, offering live scores, play-by-play updates, and interactive discussions for enthusiasts. Leveraging the NBA API, I retrieved comprehensive data for 100+ games, implementing seamless real-time updates and a chat feature using Django Channels, and WebSockets.",
+    image: "./project1-new.gif",
+    link: "http://18.118.25.177/",
+  },
+  {
+    title: "Price Comparison",
+    subtitle: "Django | HTML/CSS | AWS | Postgres",
+    description:
+      "Led a team in developing a dynamic web application enabling users to compare prices between UberEats and DoorDash and make informed decisions about their food delivery choices. Utilized PostgreSQL to create a backend database for storing user and restaurant information.",
+      image: "./pricecomparison.png",
+    link: "https://github.com/devo329/Group5-project",
+  },
+  {
+    title: "RFID Spotify Player",
+    subtitle: "Raspberry Pi | Python",
+    description:
+      "Developed a Raspberry Pi-based music player that integrated RFID tags and the Spotify API. The project allows users to  play specific songs from Spotify by scanning RFID tags, while offering convenient features such as pausing or resuming playback, queuing songs, and displaying live playback information on an OLED display.",
+      image: "https://logodix.com/logo/14886.gif",
+    link: "https://github.com/mibachcha1/RFID-Spotify-Player",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "React | Tailwind CSS",
+    description:
+      "Developed a portfolio website using React to showcase my skills, projects, and achievements. Utilized Netlify for seamless deployment and hosting. Also utilized Tailwind CSS to streamline the styling process, resulting in an engaging and visually appealing user interface.",
+      image: "./portfolio.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Python",
+  "JavaScript",
+  "Agile",
+  "HTML/CSS",
+  "React",
+  "SQL",
+  "PostgreSQL",
+  "Git",
+  "JUnit",
+  "Object-Oriented Design",
+  "AWS",
+]
+
+
---
+++ src/fonts.js
@@ -0,0 +1,6 @@
+import GoogleFonts from "google-fonts";
+
+GoogleFonts.add({
+    Rubik: true,
+    styles: true,
+  });
